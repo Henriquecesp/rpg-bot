a6bdf2edebe8aeeae706a400cca1f31eac343b39
Henrique Cruz <henrique@make.pro.br>
2020-03-27 15:54:43 -0300

    Att v0.1.1

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Rpg Bot
+
 Discord bot rpg
 
 ![Sword shield](https://i.ibb.co/w0TqSz4/rpg-game-1.png "Sword shield")
@@ -17,14 +18,17 @@ npm install
 or
 yarn
 ```
+
 create your config file called config.json
-content: 
+content:
+
 ```javascript
 {
 masterId: #admin_user_id(int)
 token: #'your discord bot token'(string)
 }
 ```
+
 with your discord user id and token from discord dev api
 
 How to get your token, create a bot and add to a serve:
@@ -32,27 +36,60 @@ How to get your token, create a bot and add to a serve:
 https://discordapp.com/developers/applications
 
 To start the bot just run:
+
 ```bash
 node .
 ```
 
 Or use nodemon for liveupload running:
+
 ```bash
 nodemon start
 ```
 
+## GUIDE
+
+> Follow this guide to use the Master Bot RPG
+
+First create your character by executing:
+`-create [CharName] [race]`
+Passing your name and function, example:
+`-create Henrique Human`
+Your character is ready! To see your character:
+`-char`
+
 ## Commands:
-- !ciar [nick] [role] = create new char on database
 
-- !char = shows your character
+-create [CharName][race] = create a new character in the database
 
-- !editar [nick] = edits your nickname
+-char = shows your character
 
-- !apagar = delete your character
+-editname [CharName] = edit character name
 
-- !quote ...text = create a quote to your character
+-editavatar = Update avatar (Saves your current discord photo)
+
+-delete = delete your character
+
+-quote ... text = create a phrase for your character
+
+-status ... text = creates status for your character
+
+-gold [CharName][value] = Edit a specific character's gold
+
+-life [CharName][value] = Edit the life of a specific character
+
+-additem [CharName][item] = Add an item to a specific character's inventory
+
+-removeitem [CharName][item] = Removes an item from a specific character's inventory
+
+-clearinventory [CharName] = Clear a specific character's inventory and create a backup
+
+-backupinventory [CharName] = Restores the inventory based on the backup created in the last clearInventory, ITEMS ADDED AFTER THE COMMAND -clearInventory WILL BE LOST
+
+**Don't forget the arguments for your commands.**
 
 ## External dependencies
+
 DiscordJS Library i used:
 <br/>
 https://discord.js.org/
@@ -60,7 +97,10 @@ https://discord.js.org/
 DataBase i used LowDb (json based db):
 <br/>
 https://github.com/typicode/lowdb
+
 ##
+
 I plan to create more methods and commands later.
+
 <hr>
 <p>Made with :coffee: and ♥ by Henrique Cruz</p>
--- bot.js
+++ bot.js
@@ -7,27 +7,27 @@ const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("banco.json");
 const db = low(adapter);
 
-const prefix = "!";
+const prefix = "-";
 const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
 client.on("ready", () => {
-  console.log(`Bot foi iniciado, em ${client.guilds.cache.size} servidores.`);
-  client.user.setActivity(`Estou pronto!`);
+  console.log(`Bot started, in ${client.guilds.cache.size} servers.`);
+  client.user.setActivity(`Im ready!`);
 });
 
 client.on("guildCreate", guild => {
   console.log(
-    `O bot entrou no servidor: ${guild.name} (id: ${guild.id}. Populacao: ${guild.memberCount} membros!)`
+    `The bot started at server: ${guild.name} (id: ${guild.id}. Population: ${guild.memberCount} members!)`
   );
   db.set(guild.id, []).write();
-  client.user.setActivity(`Estou pronto!`);
+  client.user.setActivity("Im ready!");
 });
 
 client.on("guildDelete", guild => {
   console.log(
-    `O bot foi removido do servidor: ${guild.name} (id: ${guild.id}.)`
+    `The bot was removed from server: ${guild.name} (id: ${guild.id}.)`
   );
-  client.user.setActivity(`Estou pronto!`);
+  client.user.setActivity(`Im ready!`);
 });
 
 client.on("message", async message => {
@@ -45,12 +45,10 @@ client.on("message", async message => {
     .split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  if (command === "ping") {
-    const m = await message.channel.send("Ping?");
-    m.edit(
-      `Pong! A Latencia e ${m.createdTimestamp - message.createdTimestamp}ms.
-      )}ms`
-    );
+  if (command === "teste") {
+    let server = message.guild.emojis.cache;
+    console.log(server);
+    // message.channel.send(server);
   }
 
   if (command === "char") {
@@ -58,6 +56,11 @@ client.on("message", async message => {
       .get(message.guild.id)
       .find({ id: message.author.id })
       .value();
+    //inventory cant be null
+    let inventory = [...char.inventory];
+    if (inventory[0] == null) {
+      inventory[0] = "Vazio";
+    }
     const charEmbed = {
       color: 0x0099ff,
       title: char.nick,
@@ -67,16 +70,22 @@ client.on("message", async message => {
       },
       fields: [
         {
-          name: "Status",
-          value: `<:heart:691838366039408760> Life: ${char.life}
-          <:role:691838202700627968> Role: ${char.role}
-          <:coin:691838183817609226> Money: ${char.money}`
+          name: " <:heart:691838366039408760> Status",
+          value: ` Life: ${char.life}
+          Race: ${char.race}
+          Gold: ${char.gold}`
+          //<:race:691838202700627968>
+          // <:coin:691838183817609226>
         },
         {
-          name: "Progress",
-          value: `<:xp:691841766281707550> XP: ${char.progress.xp}/${char.progress.levelxp}
-          <:level:691842752354189412> Level: ${char.progress.level}`
+          name: `<:chest:692593319548682280> Inventory`,
+          value: `${inventory}`
         }
+        // {
+        //   name: "Progress",
+        //   value: `<:xp:691841766281707550> XP: ${char.progress.xp}
+        //   <:level:691842752354189412> Level: ${char.progress.level}`
+        // }
       ],
       timestamp: new Date(),
       footer: {
@@ -86,12 +95,25 @@ client.on("message", async message => {
     message.channel.send({ embed: charEmbed });
   }
 
-  if (command === "criar") {
+  if (command === "create") {
     if (!args[(0, 1)])
       return message.channel.send(
-        "Você esqueceu do argumento, !criar [nome] [role] "
+        "You forgot the argument, !create [name] [race] "
       );
-    let [nick, role] = args;
+    let nickvalue = args[0];
+    let nick = nickvalue.toUpperCase();
+    let subtract = 8 + nick.length + 1;
+    let race = message.author.lastMessage.content;
+    race = race.substr(subtract, race.length);
+    race = race.toLowerCase();
+    race = race.charAt(0).toUpperCase() + race.slice(1);
+    let nickverify = db
+      .get(message.guild.id)
+      .find({ nick: nick })
+      .value();
+    if (nickverify != undefined) {
+      return message.channel.send("Already exist character with this name");
+    }
     let char = db
       .get(message.guild.id)
       .find({ id: message.author.id })
@@ -101,48 +123,214 @@ client.on("message", async message => {
         .push({
           id: message.author.id,
           nick: nick,
-          role: role,
+          race: race,
           life: 100,
-          money: 50,
+          gold: 50,
           progress: {
             xp: 0,
             level: 1,
             levelxp: 10
           },
-          avatar: message.author.displayAvatarURL()
+          avatar: message.author.displayAvatarURL(),
+          inventory: [],
+          backupInventory: []
         })
         .write();
-      message.channel.send(`Perfil criado`);
+      message.channel.send("Profile created");
     } else {
-      message.channel.send("Voce ja possui um perfil!");
+      message.channel.send("You already have a profile!");
     }
   }
 
-  if (command === "editar") {
-    if (!args[0]) return message.channel.send("Você esqueceu do argumento ");
-    let [newnick] = args;
+  if (command === "editname") {
+    if (!args[0])
+      return message.channel.send(
+        "You forgot the argument, !editname [newname]"
+      );
+    let value = args[0];
+    let newnick = value.toUpperCase();
+    let nickverify = db
+      .get(message.guild.id)
+      .find({ nick: newnick })
+      .value();
+    if (nickverify != undefined) {
+      return message.channel.send("Already exist character with this name");
+    }
     db.get(message.guild.id)
       .find({ id: message.author.id })
       .assign({ nick: newnick })
       .write();
-    message.channel.send("Perfil editado com sucesso!");
+    message.channel.send("Profile edited successfully!");
   }
 
-  if (command === "apagar") {
+  if (command === "editavatar") {
+    db.get(message.guild.id)
+      .find({ id: message.author.id })
+      .assign({ avatar: message.author.displayAvatarURL() })
+      .write();
+    message.channel.send("Profile edited successfully!");
+  }
+
+  if (command === "gold") {
+    if (!args[(0, 1)])
+      return message.channel.send(
+        "You forgot the argument, !gold [value] [name] "
+      );
+    let value = args[1];
+    let newgold = parseInt(value);
+    let userValue = args[0];
+    let user = userValue.toUpperCase();
+
+    const char = db
+      .get(message.guild.id)
+      .find({ nick: user })
+      .value();
+    if (char === undefined) {
+      return message.channel.send("Character does not exist");
+    }
+    newgold += char.gold;
+    db.get(message.guild.id)
+      .find({ nick: user })
+      .assign({ gold: newgold })
+      .write();
+    message.channel.send("Gold atualized " + newgold);
+  }
+
+  if (command === "life") {
+    if (!args[(0, 1)])
+      return message.channel.send(
+        "You forgot the argument, !life [name] [value] "
+      );
+    let value = args[1];
+    let newlife = parseInt(value);
+    let userValue = args[0];
+    let user = userValue.toUpperCase();
+
+    const char = db
+      .get(message.guild.id)
+      .find({ nick: user })
+      .value();
+    newlife += char.life;
+    if (newlife < 0) {
+      message.channel.send("YOU DIED");
+    }
+    db.get(message.guild.id)
+      .find({ nick: user })
+      .assign({ life: newlife })
+      .write();
+    message.channel.send("Life atualized " + newlife);
+  }
+
+  if (command === "additem") {
+    if (!args[(0, 1)])
+      return message.channel.send(
+        "You forgot the argument, !additem [charName] [item] "
+      );
+    let user = args[0];
+    user = user.toUpperCase();
+    let subtract = 9 + user.length + 1;
+    let item = message.author.lastMessage.content;
+    item = item.substr(subtract, item.length);
+    item = item.toLowerCase();
+    item = item.charAt(0).toUpperCase() + item.slice(1);
+    const char = db
+      .get(message.guild.id)
+      .find({ nick: user })
+      .value();
+    if (char == undefined) {
+      return message.channel.send("This character does not exist!");
+    }
+    let inventory = char.inventory;
+    inventory.push(item);
+    db.get(message.guild.id)
+      .find({ id: message.author.id })
+      .assign({ inventory: inventory })
+      .write();
+    message.channel.send("Item " + item + " added to inventory");
+  }
+
+  if (command === "removeitem") {
+    if (!args[(0, 1)])
+      return message.channel.send(
+        "You forgot the argument, !removeitem [charname] [item] "
+      );
+    let user = args[0];
+    user = user.toUpperCase();
+    let subtract = 12 + user.length + 1;
+    let item = message.author.lastMessage.content;
+    item = item.substr(subtract, item.length);
+    item = item.toLowerCase();
+    item = item.charAt(0).toUpperCase() + item.slice(1);
+    const char = db
+      .get(message.guild.id)
+      .find({ nick: user })
+      .value();
+    if (char == undefined) {
+      return message.channel.send("This character does not exist!");
+    }
+    let inventory = char.inventory;
+    inventory = inventory.filter(e => e !== item);
+    db.get(message.guild.id)
+      .find({ id: message.author.id })
+      .assign({ inventory: inventory })
+      .write();
+    message.channel.send("Item " + item + " removed from inventory");
+  }
+
+  if (command === "clearinventory") {
+    if (!args[0])
+      return message.channel.send(
+        "You forgot the argument, !clearinventory [charname] "
+      );
+    let user = args[0];
+    user = user.toUpperCase();
+    let char = db
+      .get(message.guild.id)
+      .find({ nick: user })
+      .value();
+    let backupInventory = char.inventory;
+    db.get(message.guild.id)
+      .find({ nick: user })
+      .assign({ inventory: [], backupInventory: backupInventory })
+      .write();
+    message.channel.send("Inventory of " + user + " cleared");
+  }
+
+  if (command === "backupinventory") {
+    if (!args[0])
+      return message.channel.send(
+        "You forgot the argument, !backupinventory [charname] "
+      );
+    let user = args[0];
+    user = user.toUpperCase();
+    let char = db
+      .get(message.guild.id)
+      .find({ nick: user })
+      .value();
+    let inventory = char.backupInventory;
+    db.get(message.guild.id)
+      .find({ nick: user })
+      .assign({ inventory: inventory, backupInventory: [] })
+      .write();
+    message.channel.send("Inventory of " + user + " restaured");
+  }
+
+  if (command === "delete") {
     db.get(message.guild.id)
       .remove({ id: message.author.id })
       .write();
-    message.channel.send("Perfil deletado");
+    message.channel.send("Profile deleted");
   }
 
   if (command === "status") {
-    if (!args[0]) return message.channel.send("Você esqueceu do argumento ");
+    if (!args[0])
+      return message.channel.send("You forgot the argument, !status [status] ");
     let [newstatus] = args;
     db.get(message.guild.id)
       .find({ id: message.author.id })
       .assign({ status: newstatus })
       .write();
-    message.channel.send("Perfil editado com sucesso!");
+    message.channel.send("Status created!");
   }
 
   if (command === "quote") {
@@ -157,7 +345,7 @@ client.on("message", async message => {
         .find({ id: message.author.id })
         .assign({ quote: newquote })
         .write();
-      message.channel.send("Perfil editado com sucesso!");
+      message.channel.send("Quote created");
     }
     const embedQuote = {
       title: char.nick,
@@ -166,35 +354,27 @@ client.on("message", async message => {
     message.channel.send({ embed: embedQuote });
   }
 
-  if (command === "xp") {
-    if (message.author.id != config.masterId) {
-      message.channel.send("Voce nao tem permissao");
-    }
-    if (!args[0]) return message.channel.send("Voce esqueceu do argumento");
-    let [newxp] = args;
-    let char = db
-      .get(message.guild.id)
-      .find({ id: message.author.id })
-      .value();
-    let newchar = {
-      progress: {
-        xp: newxp,
-        level: char.progress.level,
-        levelxp: char.progress.levelxp
-      }
-    };
-    console.log(args[0]);
-    for (let i = 0; i <= args[0]; i++) {
-      if (newchar.progress.xp >= newchar.progress.levelxp) {
-        newchar.progress.level++;
-        newchar.progress.xp = 0;
-      }
-      console.log(newchar.progress);
-    }
-    db.get(message.guild.id)
-      .find({ id: message.author.id })
-      .assign({ progress: newchar.progress })
-      .write();
+  if (command === "showall") {
+    const chars = db.get(message.guild.id).value();
+    chars.forEach(function(char) {
+      const charEmbed = {
+        color: 0x0099ff,
+        title: char.nick,
+        description: char.status,
+        thumbnail: {
+          url: char.avatar
+        },
+        fields: [
+          {
+            name: "Status",
+            value: `<:heart:691838366039408760> Life: ${char.life}
+            <:race:691838202700627968> Race: ${char.race}`
+          }
+        ],
+        timestamp: new Date()
+      };
+      return message.channel.send({ embed: charEmbed });
+    });
   }
 });
 
